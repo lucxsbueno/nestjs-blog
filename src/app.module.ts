bf15df62d0bb9a8f872bd8a161b41eaf49d96fd7
lucxsbueno <lucas_bueno2@estudante.sc.senai.br>
2025-04-22 22:07:18 -0300

    feat: new categories route and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TagsModule } from './tags/tags.module';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [TagsModule],
+  imports: [TagsModule, CategoriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
